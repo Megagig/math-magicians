2d44e8c1eeaaf4eb064c7c8f907e9a80c21ec1e5
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-07 01:09:50 +0100

    Update styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/calculator';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      somethinguseless: true,
-    };
-  }
+const calc = () => {
+  <div>
+    <h1>Calculator App</h1>
+    <Calculator />
+  </div>;
+};
 
-  render() {
-    const { somethingUseless } = this.state;
-    return <div className='App'>{somethingUseless && <Calculator />}</div>;
-  }
-}
-
-export default App;
+export default calc;
