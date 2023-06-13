d30c68e99c75704a29785fdb9e6fc2ee27f41080
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-13 18:17:27 +0100

    Add the styles for the Homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Calculator from './components/calculator';
 import GetQuotes from './components/displayQuote';
+import Home from './components/Home';
 
 function App() {
   const [calculatorObj, setCalculatorObj] = useState({
@@ -18,6 +19,7 @@ function App() {
         setCalculatorObj={setCalculatorObj}
       />
       <GetQuotes />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import styles from '../styles/Home.module.css';
+
+const Home = () => (
+  <div className={styles.container}>
+    <h1>Math Magicians</h1>
+    <h2>Welcome to our page!</h2>
+
+    <div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero itaque
+        aspernatur eligendi optio a veniam sapiente deserunt totam assumenda
+        aut, ab nemo suscipit ratione repudiandae doloremque distinctio vitae
+        reiciendis! Dolor sit voluptatem ullam necessitatibus ducimus iure
+        voluptates natus ad aspernatur? Quia quae voluptatibus neque repellat
+        quos magnam suscipit, corporis fugit.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero itaque
+        aspernatur eligendi optio a veniam sapiente deserunt totam assumenda
+        aut, ab nemo suscipit ratione repudiandae doloremque distinctio vitae
+        reiciendis! Dolor sit voluptatem ullam necessitatibus ducimus iure
+        voluptates natus ad aspernatur? Quia quae voluptatibus neque repellat
+        quos magnam suscipit, corporis fugit.
+      </p>
+    </div>
+  </div>
+);
+
+export default Home;
